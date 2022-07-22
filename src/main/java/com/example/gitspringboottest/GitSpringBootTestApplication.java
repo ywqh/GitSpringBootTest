903acd68088270a850745870b3ab5865d2e8d946
yangwenquan <2890127577@qq.com>
2022-07-22 21:55:27 +0800

    idea-git-test 1.1

--- src/main/java/com/example/gitspringboottest/GitSpringBootTestApplication.java
+++ src/main/java/com/example/gitspringboottest/GitSpringBootTestApplication.java
@@ -2,12 +2,18 @@ package com.example.gitspringboottest;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+
+import java.util.Map;
 
 @SpringBootApplication
 public class GitSpringBootTestApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(GitSpringBootTestApplication.class, args);
+        ConfigurableApplicationContext run = SpringApplication.run(GitSpringBootTestApplication.class, args);
+        ConfigurableEnvironment environment = run.getEnvironment();
+        System.out.println(environment.getSystemEnvironment());
     }
 
 }
