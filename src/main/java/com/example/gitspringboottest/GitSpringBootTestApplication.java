0ba33de8daee0245a08e45bcdda9d180192737ca
yangwenquan <2890127577@qq.com>
2022-07-22 16:07:22 +0800

    idea-git-test 1.0

---
+++ src/main/java/com/example/gitspringboottest/GitSpringBootTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.gitspringboottest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitSpringBootTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitSpringBootTestApplication.class, args);
+    }
+
+}
